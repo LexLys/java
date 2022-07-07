b792fa69b96df650115df8e00f78b7633d8a2e7d
Alex Lys <87alys@gmail.com>
2022-07-07 20:23:28 +0300

    DZ 5 second

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,10 +22,14 @@ public class Main {
 // git commit -m "DZ 5 local"
 
 // Создайте новую ветку и перейдите в неё;
-// git branch dz5
-// git checkout dz5
+// git branch newDZ5
+// git checkout newDZ5
 
 // В новой ветке создайте папку blog с файлами: index.js, index.html;
-//
+// переходим на ветку newDZ5
+// создаем папку в ней 2 файла по заданию
+// Далее прописываем:
+// git add .
+// git commit -m "DZ 5 second"
 
 
