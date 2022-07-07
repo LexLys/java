812269faa4bd8a610dd7ccd607e66ba8aced4c6b
Alex Lys <87alys@gmail.com>
2022-07-07 20:05:04 +0300

    DZ 5

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+//git init - подключаем Git
+
+// Создаем файл .gitignore
+// В файле .gitignore пишем:
+// *.sass
+// /bin
+// /admin
+// /config
+
+// Сохраняем
+
+// Добавьте все файлы в локальное хранилище;
+// git add -A
+
+
+
